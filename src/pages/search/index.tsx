b4634bcefd3321af11db8a855845e0f846a675af
Wang <wang.xuezhi@talkdesk.com>
2025-07-17 16:50:38 +0800

    fix: init code

---
+++ src/pages/search/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+interface SearchProps {
+  title?: string;
+}
+
+const Search: React.FC<SearchProps> = ({ title }) => {
+  return <div>Comp</div>;
+};
+
+export default Search;
